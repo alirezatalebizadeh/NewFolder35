b883b299bd222aceb141e779d1f88a032d500a92
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-11-05 19:01:57 +0330

    answer to question

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+let user = {
+  name: "Alireza",
+  family: "Zare",
+  age: 25,
+};
 
+delete user.age;
 
+// console.log(user);
+//{
+//"name": "Alireza",
+//"family": "Zare"
+//}
+let number ={
+    item:12
+}
 
+delete number.item
+//delete can not delete varable
 
-
-
-
-
-
-
-
-
-
+const result= number.item
+console.log(result);//=> undefined
