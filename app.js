50fd0a8b487ba3966b8b40eb3603375dfb99fad2
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-11-06 08:56:30 +0330

    hoisting and iife and number

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 
-function addNumbers(){
-    let sum = 0;
-    Array.from(arguments).forEach(num => {
-        sum +=  num
-    });
-    return sum
-}
 
-// addNumbers(1,2,3,4)//Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
 
-console.log(addNumbers(1,2,3,4,5,6,7,8,9));
+
+
+
+
 
